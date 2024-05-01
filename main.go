66ff1ecd88a91b90d0b4d14c44a7dcbd487299c1
Haroon Albar <haroonalbarx@gmail.com>
2024-05-01 15:51:43 +0530

    chore added discription

--- main.go
+++ main.go
@@ -1,21 +1,35 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
+// fucnction to handle home path
 func home(w http.ResponseWriter ,r *http.Request ) {
   w.Write([]byte("Boi"))
 }
 
+func bro(w http.ResponseWriter ,r *http.Request ) {
+  w.Write([]byte("Sup Bro!"))
+}
+
 func main(){
+  // create a new mux
+  // mux is essentionaly a router or http live multiplexer/server
+
   mux:= http.NewServeMux()
+
+  // to handle the root route with home function
   mux.HandleFunc("/",home)
-  fmt.Println("Hello boss")
+  // handle for /bro route using bro function
+  mux.HandleFunc("/bro",bro)
+  
 
   log.Print("Starting server on port 4000")
+
+  // listen to listen to http live server/multiplexer aka mux
   err:=  http.ListenAndServe(":4000",mux)
+  // if there is err log it
   log.Fatal(err)
 }
