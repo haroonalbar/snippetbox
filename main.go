ad5a36776c4b8b988a1b428a48f131d0eb4e4f8f
Haroon Albar <haroonalbarx@gmail.com>
2024-05-01 15:40:29 +0530

    create and handle a server

--- main.go
+++ main.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func home(w http.ResponseWriter ,r *http.Request ) {
+  w.Write([]byte("Boi"))
+}
 
 func main(){
+  mux:= http.NewServeMux()
+  mux.HandleFunc("/",home)
   fmt.Println("Hello boss")
 
+  log.Print("Starting server on port 4000")
+  err:=  http.ListenAndServe(":4000",mux)
+  log.Fatal(err)
 }
