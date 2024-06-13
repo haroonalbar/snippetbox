8350df7a9e2d35062d0214f3bb78434915d1af65
Haroon Albar <haroonalbarx@gmail.com>
2024-06-13 22:58:54 +0530

    feat: added more handlers

--- main.go
+++ main.go
@@ -6,13 +6,23 @@ import (
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Sup brodi!"))
+func handlerHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Snippetbox home page."))
+}
+
+func handleSnippetView(w http.ResponseWriter, r *http.Request){
+  w.Write([]byte("Display a specific snippet..."))
+}
+
+func handleSnippetCreate(w http.ResponseWriter, r *http.Request){
+  w.Write([]byte("Create a snippet..."))
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/", handlerHome)
+	mux.HandleFunc("/snippet/view", handleSnippetView)
+	mux.HandleFunc("/snippet/create", handleSnippetCreate)
 
   fmt.Println("Start server on port 4000")
 	err := http.ListenAndServe(":4000", mux)
