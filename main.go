0a9003ae795000b29a34ee77efc932ceea3cff02
Haroon Albar <haroonalbarx@gmail.com>
2024-06-14 15:13:09 +0530

    get parameter on sinppet view

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func handlerHome(w http.ResponseWriter, r *http.Request) {
@@ -15,13 +16,21 @@ func handlerHome(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleSnippetView(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a specific snippet..."))
+	// extract id parameter and check it's postive number
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+  // formatted http.ResponseWriter
+	fmt.Fprintf(w, "View snipped of id: %d", id)
 }
 
 func handleSnippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allowed", "POST")
-    http.Error(w,"Method Not Allowed",http.StatusMethodNotAllowed)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 	w.Write([]byte("Create a snippet..."))
